b19c237e3f328b793146ce778c06f8805f66c368
ijeomaemeruwa <helloijeoma@gmail.com>
2021-02-23 22:39:58 +0000

    update root files

--- src/App.js
+++ src/App.js
@@ -7,55 +7,52 @@ import Register from './views/Register/Register';
 import Login from './views/Login/Login';
 import Home from './views/Home/Home';
 import Start from './views/Start/Start';
+// import QuizList from './views/Quiz/QuizList'
 import Quiz from './views/Quiz/Quiz';
 import Admin from './views/Admin/Admin';
 
 
 const App = () => {
-  const { isAuthenticated, role } = useSelector((state) => state.user);
+const { isAuthenticated, role } = useSelector((state) => state.user);
 
 return (
 <>
 <Router>
 <Switch>
   <Route exact path="/"><Home /></Route>
-
   <Route exact path="/register">
-   {isAuthenticated ? <Redirect to="/" /> : <Register />}
+   {isAuthenticated ? <Redirect to="/login" /> : <Register />}
   </Route>
-
   <Route exact path="/login">
-   {isAuthenticated && role === "User" ? <Redirect to="/start" /> : <Login />}
+   {
+    isAuthenticated && role === "User" ? <Redirect to="/start" /> : 
+    isAuthenticated && role === "Admin" ? <Redirect to="/admin" /> :
+    (<Login />)
+   }
   </Route>
-
   <Route exact path="/start">
-  {isAuthenticated && role === "User" ? 
-    (<Start />) : 
-    isAuthenticated && role === "Admin" ? 
-    (<Redirect to="/" />) : 
-    (<Redirect to="/login" />)}
+  {
+    isAuthenticated && role === "User" ? <Start /> : 
+    isAuthenticated && role === "Admin" ? <Redirect to="/" /> : 
+    (<Redirect to="/login" />)
+  }
   </Route>
-
   <Route exact path="/quiz">
   {
-    isAuthenticated && role === "User" ? 
-    (<Quiz />) : 
-    isAuthenticated && role === "Admin" ? 
-    (<Redirect to="/admin" />) : 
+    isAuthenticated && role === "User" ? <Quiz /> :  
     (<Redirect to="/" />)
   }
   </Route>
   <Route exact path="/admin">
   {
-   isAuthenticated && role === "Admin" ? 
-   (<Admin />) : 
-   (<Redirect to="/" />)
+  isAuthenticated && role === "Admin" ? <Admin /> : 
+  (<Redirect to="/" />)
   }
   </Route>
 </Switch>   
 </Router>
 </>
-  );
+);
 }
 
 export default App;
